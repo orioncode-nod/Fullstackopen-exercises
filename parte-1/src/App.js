1900d1e7e1c7aa604e70db3a640350652f042ea5
Efrain Paz <efrain.e.dlp@gmail.com>
2025-10-05 03:34:46 -0300

    Parte 1.8: Componente Statistics creado (Modulación)

--- parte-1/src/App.js
+++ parte-1/src/App.js
@@ -1,63 +1,74 @@
 //import logo from './logo.svg';
 //import './App.css';
 
-const Header = (props) => {
-  return (
-    <h1>{props.course.name}</h1>
-  )
-}
+import { useState } from 'react' 
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
-
-const Content = (props) => {
+const Statistics = (props) => {
   return (
     <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <h2>statistics</h2>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.total}</p>
+      <p>average {props.average.toFixed(2)}</p>
+      <p>positive {props.positive.toFixed(2)} %</p>
     </div>
   )
 }
 
-const Total = (props) => {
-  return (
-    <p>
-      Número de ejercicios: {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-    </p>
-  )
-}
-
 const App = () => {
-  // Datos del curso
-  const course = {
-    name: 'Half Stack application development',
-    parts : [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  } 
-  
+  // 1. Define las variables de estado para los tres contadores
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  // 2. Define las funciones que actualizan el estado
+  // CLAVE: Las funciones usan la función set* para cambiar el valor
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+  //nuevo (1.7)
+  const total = good + neutral + bad
+  const average = (good -bad)/ total
+  const positive = (good / total) * 100
+
   return (
     <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>give feedback</h1>
+      
+      {/* 3. Botones: Asigna las funciones a los eventos onClick */}
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+
+      <h2>statistics</h2>
+      
+      {/* 4. Muestra los valores de estado actuales */}
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      {/*5. Muestra las nuevas estadisticas*/}
+      <p>all {total}</p>
+      <p>average{average.toFixed(2)}</p>
+      <p>positive{positive.toFixed(2)}% </p>
+      {/* Nuevo componente */}
+      <Statistics 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      />
     </div>
   )
 }
