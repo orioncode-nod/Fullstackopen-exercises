49e442495e47018fb28369441f15a14e933d3708
Efrain Paz <efrain.e.dlp@gmail.com>
2025-10-05 01:41:38 -0300

    Inicialización del repositorio único del curso. Ejercicios 1.1-1.5

---
+++ parte-1/src/App.js
@@ -0,0 +1,65 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+const Header = (props) => {
+  return (
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Número de ejercicios: {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+    </p>
+  )
+}
+
+const App = () => {
+  // Datos del curso
+  const course = {
+    name: 'Half Stack application development',
+    parts : [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  } 
+  
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
